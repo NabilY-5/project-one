4e85487d7b6e421d0114ea787e6046b2aa75f6d8
NabilY-5 <nabilyahya59280@gmail.com>
2025-03-27 14:43:43 +0100

    récup

--- script.js
+++ script.js
@@ -161,7 +161,10 @@ const showQuestion = () => {
 [reponse1, reponse2, reponse3, reponse4].forEach(function (element) {
   element.addEventListener("click", () => {
     /*chercher à ne plus cliquer sur les autres reponses*/
-    /*chercher à ne plus cliquer sur les autres reponses*/
+    [reponse1, reponse2, reponse3, reponse4].forEach((btn) => {
+      btn.style.pointerEvents = "none";
+    });
+
     if (element.textContent == questions[currentQuestionIndex].good) {
       element.style.backgroundColor = "green";
       element.style.color = "white";
@@ -169,6 +172,7 @@ const showQuestion = () => {
     } else {
       element.style.backgroundColor = "red";
       element.style.color = "white";
+
       [reponse1, reponse2, reponse3, reponse4].forEach((response) => {
         if (response.textContent === questions[currentQuestionIndex].good) {
           response.style.backgroundColor = "green";
@@ -201,6 +205,9 @@ const nextQ = () => {
       showQuestion();
       resetColors();
       nextButton.style.display = "none";
+      [reponse1, reponse2, reponse3, reponse4].forEach((btn) => {
+        btn.style.pointerEvents = "auto";
+      });
     } else {
       ShowScore();
       console.log(ShowScore);
